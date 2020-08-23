63320be236fa75a2a7e9f94ebd8d5ac248457bc8
Bhaviktutorials <64035221+Bhaviktutorials@users.noreply.github.com>
2020-08-23 10:47:56 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 clear
 echo " "
 echo " "
@@ -7,22 +7,18 @@ read varbanner
 
 echo " " 
 
-
 echo "toilet -f slant '           $varbanner'  -F gay | lolcat" > t-ban.txt
-
 
 echo -e "\e[33mEnter Your Terminal Name \e[32m  \n\n"
 read varterm
 
 echo " " 
 
-
-
 echo "PS1='\[\e[1;34m
 \a┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
 \a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
 \a├─[\033[1;32m\w\033[1;34m]\033[1;34m
-└─>$varterm[~]:# ' " > ash.txt
+\[\e[34m\]└─>$varterm[~]:#\[\e[1;92m\] ' " >ash.txt 
 
 cat "t-ban.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
